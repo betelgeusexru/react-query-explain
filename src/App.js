0d37d0538432e2c7fe2757af054aca62fd207e78
Boris Lebedev <boris.lebedev.primary@mail.ru>
2022-05-13 18:29:36 +0500

    parallel

--- src/App.js
+++ src/App.js
@@ -1,49 +1,45 @@
-import React from "react";
-import { useQuery } from "react-query";
+import React, { useState } from "react";
+import { useQueries, useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useLoad } from "./useLoad";
 
+const ids = [1, 2];
+
 function App() {
-  const { isLoading, isSuccess, data, isError } = useQuery({
-    queryKey: "super-heros",
-    queryFn: () => {
-      // return fetch("https://swapi/api/people").then((data) => data.json());
-      return fetch("https://swapi.dev/api/people").then((data) => data.json());
-    },
-    onSuccess: () => {
-      const notify = () => toast.success("all data is fechted");
-      notify();
-    },
-    onError: () => {
-      const notify = () => toast.error("server is down");
-      notify();
-    },
+  const [start, setStart] = useState(false);
 
-    refetchOnMount: true,
-    refetchIntervalInBackground: true,
-    refetchOnReconnect: true,
-    refetchInterval: 5000,
-
-    staleTime: 300000,
-    retry: false,
-  });
-
-  // const { isLoading, isSuccess, data, isError } = useLoad();
+  const result = useQueries(
+    ids.map((id) => {
+      return {
+        queryFn: () =>
+          fetch(`https://swapi.dev/api/planets/${id}`).then((data) =>
+            data.json()
+          ),
+        queryKey: `planets-with-id-${id}`,
+        enabled: start === true,
+        cacheTime: 0,
+      };
+    })
+  );
 
   return (
     <div>
-      <h1>starwars page</h1>
+      refetch{" "}
+      <button onClick={() => setStart((p) => !p)}>
+        click and check console
+      </button>
+      {start &&
+        result.map((res) => {
+          if (res.isSuccess) {
+            return <div>{res.data.name}</div>;
+          }
 
-      <Link to="/some">go to some page</Link>
-
-      <ToastContainer />
-
-      <h4>heros: </h4>
-      {isLoading && <h1>loading...</h1>}
-      {isSuccess && data.results.map((hero) => <div>{hero.name}</div>)}
-      {isError && <div>error has occured</div>}
+          if (res.isLoading) {
+            return <div>loading</div>;
+          }
+        })}
     </div>
   );
 }
