70e20951d02fb5ba53aaa8a0ad339aeea75fdd56
Boris Lebedev <boris.lebedev.primary@mail.ru>
2022-05-14 15:22:19 +0500

    base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery } from "react-query";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const { isLoading, isSuccess, data, isError } = useQuery({
+    queryKey: "super-heros",
+    queryFn: () => {
+      // return fetch("https://swapi/api/people").then((data) => data.json());
+      return fetch("https://swapi.dev/api/people").then((data) => data.json());
+    },
+    onSuccess: () => {
+      const notify = () => toast.success("all data is fechted");
+      notify();
+    },
+    onError: () => {
+      const notify = () => toast.error("server is down");
+      notify();
+    },
+
+    retry: false,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>starwars page</h1>
+
+      <ToastContainer />
+
+      <h4>heros: </h4>
+      {isLoading && <h1>loading...</h1>}
+      {isSuccess && data.results.map((hero) => <div>{hero.name}</div>)}
+      {isError && <div>error has occured</div>}
     </div>
   );
 }
